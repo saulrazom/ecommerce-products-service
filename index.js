9e7317c517e3e9db90818da925568e66b7f0dc92
Saul Razo <saul.razo@iteso.mx>
2026-03-10 10:36:11 -0600

    test

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.use('/products', productRoutes)
 
 if (require.main === module) {
   const PORT = process.env.PORT || 3000
-  app.listen(PORT, () => {
+  app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on http://localhost:${PORT}`)
   })
 }
